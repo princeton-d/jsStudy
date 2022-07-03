a22e275c058f38576f1e972e232f39299d563e7c
websteak <ssk0080@gmail.com>
2022-07-03 20:17:19 +0900

    로그인 기능, 모스부호 변환기(영어->모스부호)기능 추가

---
+++ assets/js/morse-code.js
@@ -0,0 +1,38 @@
+'use strict';
+// querySelector
+const morseInputForm = document.querySelector('#morse-input-form')
+const morseInputInput = document.querySelector('#morse-input-input')
+const morseOutputArea = document.querySelector('#morse-output-area')
+
+// 변수, 상수
+let morseArr;
+
+// morseCodeArea
+const morseCode = { '\u00a0': ' ', a: '·--', b: '-···', c: '-·-·', d: '-··', e: '·', f: '··-·', g: '--·', h: '····', i: '··', j: '·---', k: '-·-', l: '·-··', m: '--', n: '-·', o: '---', p: '·--·', q: '--·-', r: '·-·', s: '···', t: '-', u: '··-', v: '···-', w: '·--', x: '-··-', y: '-·--', z: '--··', zero: '-----', one: '·----', two: '··---', three: '···--', four: '····-', five: '·····', six: '-····', seven: '--···', eight: '---··', nine: '----·', }
+
+// event
+morseInputForm.addEventListener('submit', startPreventDefault);
+
+function startPreventDefault(event) {
+  event.preventDefault();
+  morseText()
+}
+function morseText() {
+  const morseArr = morseInputInput.value.split('').map(String)
+  changedMorse()
+
+  function changedMorse() {
+    for (let i = 0; i < morseArr.length; i++) {
+      morseArr[i] = morseCode[morseArr[i]];
+    }
+    outPutMorse(morseArr)
+
+    function outPutMorse(morse) {
+      let outPutText = '';
+      for (let i = 0; i < morse.length; i++) {
+        morse[i] === undefined ? outPutText += '\u00a0\u00a0\u00a0\u00a0\u00a0' : outPutText += `${morse[i]} `
+      }
+      morseOutputArea.innerText = outPutText;
+    }
+  }
+}
